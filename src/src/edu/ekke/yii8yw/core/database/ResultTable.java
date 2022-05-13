798f707cd3d781916942ca15d44318fe08b252d5
Jámbor Alexandra <ja20000907@gmail.com>
2022-05-13 20:56:27 +0200

    Implemented ResultTable utility class

---
+++ src/src/edu/ekke/yii8yw/core/database/ResultTable.java
@@ -0,0 +1,83 @@
+package edu.ekke.yii8yw.core.database;
+
+import edu.ekke.yii8yw.helpers.List2;
+import java.util.HashMap;
+
+public class ResultTable {
+    private List2<String> fieldNames = new List2<>();
+    private List2<List2<Object>> records = new List2<>();
+
+    public List2<String> getFieldNames() {
+        return fieldNames;
+    }
+
+    public void addField(String columnName) {
+        fieldNames.add(columnName);
+    }
+
+    public void add(List2<Object> list) {
+        records.add(list);
+    }
+
+    public HashMap<String, Object> getRecord(int index){
+        var result = new HashMap<String, Object>();
+        for (int i = 0; i < fieldNames.size(); i++) {
+            try {
+                result.put(fieldNames.get(i), records.get(index).get(i));
+            } catch (IndexOutOfBoundsException ignored) {
+                result.put(fieldNames.get(i), null);
+            }
+        }
+        return result;
+    }
+
+    public List2<Object> getFrom(String fieldName) {
+        var result = new List2<>();
+        var index = fieldNames.indexOf(fieldName);
+        if(index == -1) {
+            return result;
+        } else{
+            for (var item: records) {
+                try {
+                    result.add(item.get(index));
+                } catch (IndexOutOfBoundsException ignored) {
+                    result.add(null);
+                }
+            }
+        }
+        return result;
+    }
+
+    public List2<HashMap<String, Object>> all() {
+        var result = new List2<HashMap<String, Object>>();
+        for (var record: records) {
+            var temp = new HashMap<String, Object>();
+            for (int col = 0; col < record.size(); col++) {
+                try {
+                    temp.put(fieldNames.get(col), record.get(col));
+                } catch (IndexOutOfBoundsException ignored) {
+                }
+            }
+            result.add(temp);
+        }
+        return result;
+    }
+
+    public String getFieldName(int i) {
+        return fieldNames.get(i);
+    }
+
+    public HashMap<String, Object> first(){
+        var temp = new HashMap<String, Object>();
+        try {
+            var record = records.get(0);
+            for (int col = 0; col < record.size(); col++) {
+                try {
+                    temp.put(fieldNames.get(col), record.get(col));
+                } catch (IndexOutOfBoundsException ignored) {
+                }
+            }
+        }catch (Exception ignored){ }
+        return temp;
+    }
+}
